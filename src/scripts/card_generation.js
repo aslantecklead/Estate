6ccbb9d87abca7a5813c07cbc41ca48d3be28574
Aslan <isip_a.g.asalbekov@mpt.ru>
2023-11-08 15:07:02 +0300

    Styled cards and headline with font

--- src/scripts/card_generation.js
+++ src/scripts/card_generation.js
@@ -2,18 +2,35 @@ const cardContainer = document.getElementById("cardContainer");
 
 async function loadDataFromServer() {
     try {
-        const response = await fetch('http://localhost:3000/');
+        const response = await fetch('http://localhost:3001/properties');
         if (response.ok) {
             const data = await response.json();
             const cardData = data.data;
 
             if (cardData) {
-                const cardTemplateSource = document.getElementById("card-template").innerHTML;
-                const cardTemplate = Handlebars.compile(cardTemplateSource);
+                cardContainer.innerHTML = ''; 
 
                 cardData.forEach((data) => {
-                    const cardHtml = cardTemplate(data);
-                    cardContainer.innerHTML += cardHtml;
+                    const cardDiv = document.createElement('div');
+                    cardDiv.className = 'col-md-4 card-container';
+                    cardDiv.innerHTML = `
+                        <div class="card mx-auto">
+                            <div class="card-image">
+                                <img src="${data.imageSrc}" class="card-img" alt="Property Image">
+                            </div>
+                            <div class="card-body d-flex justify-content-between align-items-center">
+                                <div>
+                                    <h5 class="price">${data.price}</h5>
+                                    <div class="details">
+                                        <p class="name">${data.address}</p>
+                                        <p class="slash"> | </p>
+                                        <p class="broker_name">${data.brokerName}</p>
+                                    </div>
+                                </div>
+                             </div>
+                        </div>
+                    `;
+                    cardContainer.appendChild(cardDiv);
                 });
             } else {
                 console.error('No data received from the server');

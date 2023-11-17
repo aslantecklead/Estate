bcf26e614ef81d10734cb64a0f763d019dbec175
Aslan <isip_a.g.asalbekov@mpt.ru>
2023-11-17 15:45:43 +0300

    Точки на карте

--- src/scripts/Maps/yamaps.js
+++ src/scripts/Maps/yamaps.js
@@ -17,3 +17,30 @@ function init() {
 
     myMap.setType('yandex#map');
 }
+
+async function loadDataFromServer() {
+    try {
+        const response = await fetch('http://localhost:3001/estatelist');
+        if (!response.ok) {
+            throw new Error('Failed to fetch data:', response.status);
+        }
+        const pins = await response.json();
+        if (!pins || pins.length === 0) {
+            throw new Error('No data received from the server');
+        }
+        pins.forEach((data) => {
+            const coordinates = [data.latLong.latitude, data.latLong.longitude];
+            const price = data.price;
+            const priceText = `Price: ${price}`;
+        
+            const placemark = new ymaps.Placemark(coordinates, {
+                iconContent: priceText, 
+            });
+            myMap.geoObjects.add(placemark);
+        });
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+window.addEventListener('load', loadDataFromServer);

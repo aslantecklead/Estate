6af843a2b400cad45d2ed9034f42b92511abf112
Aslan <isip_a.g.asalbekov@mpt.ru>
2023-11-27 00:36:27 +0300

    докерфайл

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3001;
 const fs = require('fs');
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
 
 app.use(express.json());
 

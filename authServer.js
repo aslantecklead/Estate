8df0db0d3ab8679737a0042d8646238d8fcb548a
Aslan <isip_a.g.asalbekov@mpt.ru>
2023-12-01 21:46:40 +0300

    два сервера, один для авторизации просто

--- authServer.js
+++ authServer.js
@@ -2,11 +2,7 @@ require('dotenv').config()
 
 const express = require('express');
 const app = express();
-const port = 3001;
-const fs = require('fs');
 const jwt = require('jsonwebtoken');
-
-// app.use(bodyParser.json());
 
 app.use(express.json());
 
@@ -20,41 +16,16 @@ app.get('/auth', (req, res) => {
   res.sendFile(__dirname + '/Auth.html');
 });
 
-let dataset;
-
-function getDataset() {
-  return dataset;
-}
-
-app.get('/estatelist', async (req, res) => {
-  try {
-    const data = fs.readFileSync('./src/scripts/Dependencies/GetSearchPageState.json', 'utf8');
-    dataset = JSON.parse(data);
-    res.json(dataset);
-    
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Failed to read or parse JSON data' });
-  }
-});
-
-// app.listen(port, () => {
-//   console.log(`Сервер запущен на порту ${port}`);
-// });
-
-const posts = [
-  {
-    username: 'Aslan',
-    title: 'Post 1'
-  }, 
-  {
-    username: 'Xenia',
-    title: 'Post 2'
-  }
-]
-
 app.get('/posts', authenticateToken,(req, res) => {
   res.json(posts.filter(posts => posts.username === req.user.name));
+});
+
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  const user = { name: username };
+  
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+  res.json({ accessToken: accessToken });
 });
 
 function authenticateToken(req, res, next) {
@@ -69,4 +40,6 @@ function authenticateToken(req, res, next) {
   })
 }
 
-app.listen(3001);
+
+
+app.listen(4000);

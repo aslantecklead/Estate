5cf25e548cce48d4b6839628046b24637439c474
Aslan <isip_a.g.asalbekov@mpt.ru>
2023-12-01 18:48:23 +0300

    настроили зависимости

--- index.js
+++ index.js
@@ -1,40 +1,5 @@
-const express = require('express');
-const app = express();
-const port = 3001;
-const fs = require('fs');
-const fetch = require('node-fetch');
-
-app.use(express.json());
-
-app.use(express.static(__dirname + '/src'));
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
-app.get('/auth', (req, res) => {
-  res.sendFile(__dirname + '/Auth.html');
-});
-
-let dataset;
-
-function getDataset() {
-  return dataset;
-}
-
-app.get('/estatelist', async (req, res) => {
-  try {
-    const data = fs.readFileSync('./src/scripts/Dependencies/GetSearchPageState.json', 'utf8');
-    dataset = JSON.parse(data);
-    res.json(dataset);
-    
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Failed to read or parse JSON data' });
-  }
-});
+const { app, port } = require('/server');
 
 app.listen(port, () => {
-  console.log(`Сервер запущен на порту ${port}`);
+  // console.log(`Сервер запущен на порту ${port}`);
 });
-

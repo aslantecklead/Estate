38f4a0c21b2fe158162e4ef1d2579137b924bb1d
Aslan <isip_a.g.asalbekov@mpt.ru>
2023-12-19 10:18:13 +0300

    add

--- src/scripts/Admin/create_backup.js
+++ src/scripts/Admin/create_backup.js
@@ -1,17 +1,34 @@
-const backupButton = document.getElementById('backupButton');
-
-backupButton.addEventListener('click', async () => {
-  try {
-    const response = await fetch('/createBackup', {
-      method: 'POST',
-    });
-
-    if (response.ok) {
-      console.log('Запрос на создание бэкапа успешно отправлен');
-    } else {
-      console.error('Ошибка при создании бэкапа');
+document.getElementById('exportButton').addEventListener('click', async () => {
+    try {
+      const response = await fetch('/propertyData'); // Запрос на получение данных
+      if (!response.ok) {
+        throw new Error('Ошибка при получении данных');
+      }
+  
+      const data = await response.json(); // Получение данных в формате JSON
+  
+      const csvHeaders = Object.keys(data[0]).join(',') + '\n'; // Создание заголовков CSV
+  
+      const csvRows = data.map(obj =>
+        Object.values(obj)
+          .map(value => (typeof value === 'string' ? `"${value}"` : value))
+          .join(',')
+      ).join('\n'); // Преобразование объектов в строки CSV
+  
+      const csvContent = csvHeaders + csvRows;
+  
+      const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+      const link = document.createElement('a');
+      if (window.navigator.msSaveOrOpenBlob) {
+        navigator.msSaveBlob(blob, 'propertyData.csv');
+      } else {
+        link.href = window.URL.createObjectURL(blob);
+        link.setAttribute('download', 'propertyData.csv');
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      }
+    } catch (error) {
+      console.error('Произошла ошибка:', error);
     }
-  } catch (error) {
-    console.error('Ошибка:', error);
-  }
-});
+  });
